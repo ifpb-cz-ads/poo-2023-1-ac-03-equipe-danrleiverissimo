ebd7c1e8c23b1a0b0fb07af21fe319f190cca6cc
Danrlei <dxnrleilira@gmail.com>
2023-03-28 22:18:02 -0300

    Correção da atividade pratica 2 do livro CBB

--- Q02_CBB.java
+++ Q02_CBB.java
@@ -1,15 +1,15 @@
 class Q02_CBB{
     public static void main(String args[]){
         // Declaração das variáveis
-        double valor1 = 10.5;
-        double valor2 = 5.2;
-        double valor3 = 7.8;
+        int valor1 = 10;
+        int valor2 = 5;
+        int valor3 = 7;
         //Cálculo da Soma
-        double soma = valor1 + valor2 + valor3;
+        int soma = valor1 + valor2 + valor3;
         // Impressão do resultado
         System.out.println("A soma é: " + soma);
         // Cálculo da média
-        double media = (valor1 + valor2 + valor3) / 3;
+        double media = (double)(valor1 + valor2 + valor3) / 3;
         // Impressão do resultado
         System.out.println("A média é: " + media);
     }
